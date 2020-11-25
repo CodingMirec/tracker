8da101db0b994df4b2cbd924272f32430f74de7a
CodingMirec <m.sykora01@gmail.com>
2020-11-25 22:34:51 +0100

    forget return() :D, displayed succes..

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Login from "../../../../Clones/Amazon-clone-project/amazon-clone/src/Login";
 
 import "./App.css";
+import Login from "./Login";
 
 function App() {
-  <Router>
-    <div className="app">
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-      </Switch>
-    </div>
-  </Router>;
+  return (
+    <Router>
+      <div className="app">
+        <h1>STH</h1>
+        <Switch>
+          <Route path="/login">
+            <h1>heh</h1>
+            <Login />
+          </Route>
+          <Route path="/">
+            <h1>Default</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { Link, useHistory } from "react-router-dom";
 
 import "./Login.css";
 
 function Login() {
-  return <div></div>;
+  const history = useHistory();
+
+  return (
+    <div className="login">
+      <h1>Login</h1>
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt="amazon_logo"
+        />
+      </Link>
+      <div className="login__container">
+        <h1>Sign In</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
