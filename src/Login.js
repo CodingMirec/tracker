230a9b797c3ea0940af5d0b57b63755068360b5f
CodingMirec <m.sykora01@gmail.com>
2020-11-28 00:09:02 +0100

    mui checkbox

--- src/Login.js
+++ src/Login.js
@@ -3,6 +3,9 @@ import { useHistory } from "react-router-dom";
 import { Button } from "@material-ui/core";
 
 import "./Login.css";
+import LockOpenIcon from "@material-ui/icons/LockOpen";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 function Login() {
   const history = useHistory();
@@ -18,13 +21,18 @@ function Login() {
   return (
     <div className="login">
       <h1>Login</h1>
-      <form>
-        <input placeholder="Email Adress" name="email" type="email" />
-        <input placeholder="Password" type="password" />
-        <label htmlFor="remember">
-          <input type="checkbox" />
-          Remember me
-        </label>
+      <LockOpenIcon />
+      <form className="login__form">
+        <div className="login__form__info">
+          <input placeholder="Email Adress" name="email" type="email" />
+          <input placeholder="Password" type="password" />
+
+          <FormControlLabel
+            value="remember"
+            control={<Checkbox color="primary" />}
+            label="Remember me"
+          />
+        </div>
         <Button ocClick={login}>Sign In</Button>
       </form>
     </div>
