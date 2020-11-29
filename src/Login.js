74aed2dcba822fc9e0ca66363fe67be9b7679250
CodingMirec <m.sykora01@gmail.com>
2020-11-29 18:08:06 +0100

    shape done

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import { Button } from "@material-ui/core";
 
 import "./Login.css";
@@ -20,21 +20,25 @@ function Login() {
 
   return (
     <div className="login">
-      <h1>Login</h1>
-      <LockOpenIcon />
-      <form className="login__form">
-        <div className="login__form__info">
-          <input placeholder="Email Adress" name="email" type="email" />
-          <input placeholder="Password" type="password" />
-
-          <FormControlLabel
-            value="remember"
-            control={<Checkbox color="primary" />}
-            label="Remember me"
-          />
-        </div>
-        <Button ocClick={login}>Sign In</Button>
-      </form>
+      <div className="login__container">
+        <h1>Login</h1>
+        <LockOpenIcon />
+        <form className="login__form">
+          <div className="login__form__info">
+            <input placeholder="Email Adress" name="email" type="email" />
+            <input placeholder="Password" type="password" />
+            <div className="login__form__account">
+              <FormControlLabel
+                value="remember"
+                control={<Checkbox color="primary" />}
+                label="Remember me"
+              />
+              <Link to="/signUp">New Account?</Link>
+            </div>
+          </div>
+          <Button onClick={login}>Sign In</Button>
+        </form>
+      </div>
     </div>
   );
 }
